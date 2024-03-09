47d3a083aa8791d8caa0f92f881db63a13f1f4a0
Dipeshxman11 <dipeshramane9378>
2024-03-09 21:28:45 +0530

    Cacching with redis

--- server.js
+++ server.js
@@ -4,22 +4,40 @@ const mongoose = require('mongoose');
 const Task = require("./models/task")
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
-const cors = require('cors'); 
-const path = require('path'); 
+const cors = require('cors');
+const path = require('path');
 const amqp = require('amqplib/callback_api');
+const redis = require('redis');
+
+const redisClient = redis.createClient();
+
+redisClient.on('error', (err) => {
+  console.error('Error connecting to Redis:', err);
+});
+
+redisClient.on('ready', () => {
+  console.log('Connected to Redis');
+});
+
+redisClient.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://dipesh19971102:0987654321@cluster0.vsbz19n.mongodb.net/IdeaClan', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => {
-  console.log('MongoDB connected');
-})
-.catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-  process.exit(1); // Exit the process with error code 1
-});
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit the process with error code 1
+  });
+
+  
 
 // Connect to RabbitMQ
 amqp.connect('amqp://localhost', (error, connection) => {
@@ -36,7 +54,7 @@ amqp.connect('amqp://localhost', (error, connection) => {
       return;
     }
 
-    channel = ch; 
+    channel = ch;
 
     const queue = 'tasks';
     channel.assertQueue(queue, { durable: false });
@@ -44,38 +62,31 @@ amqp.connect('amqp://localhost', (error, connection) => {
 
     channel.consume('tasks', async (msg) => {
       try {
-        const { action, id, task } = JSON.parse(msg.content.toString());
+        const { action, id } = JSON.parse(msg.content.toString());
         switch (action) {
-          case 'create':
-            const newTask = new Task(task);
-            await newTask.save();
-            console.log('Task created:', newTask);
-            break;
           case 'toggle':
-            const existingTask = await Task.findById(id);
-            if (!existingTask) {
+            const taskToToggle = await Task.findById(id);
+            if (!taskToToggle) {
               throw new Error('Task not found');
             }
-            existingTask.completed = !existingTask.completed;
-            await existingTask.save();
-            console.log('Task toggled:', existingTask);
+            taskToToggle.completed = !taskToToggle.completed;
+            await taskToToggle.save();
+            console.log('Task toggled:', taskToToggle);
             break;
           case 'delete':
             await Task.findByIdAndDelete(id);
             console.log('Task deleted:', id);
             break;
-          default:
-            console.log('Unknown action:', action);
+
         }
         channel.ack(msg); // Acknowledge message
       } catch (error) {
         console.error('Error processing message:', error.message);
-        channel.reject(msg, false); 
+        channel.reject(msg, false);
       }
     });
   });
 });
-
 
 // GraphQL schema
 const schema = buildSchema(`
@@ -114,37 +125,33 @@ const root = {
         console.log('Task already exists:', existingTask);
         return existingTask;
       }
-  
+
       const task = new Task({ ...input, completed: false });
       await task.save();
-    
+
       // Publish a message to the 'tasks' queue
       console.log('Sending createTask message to RabbitMQ');
       channel.sendToQueue('tasks', Buffer.from(JSON.stringify({ action: 'create', task: { ...task.toObject(), _id: undefined } })));
       console.log('createTask message sent to RabbitMQ');
-    
+
       return task;
     } catch (error) {
       console.error('Error creating task:', error);
       throw error;
     }
   },
-  
-  
+
+
   toggleTask: async ({ id }) => {
-    const task = await Task.findById(id);
-    task.completed = !task.completed;
-    await task.save();
 
     // Publish a message to the 'tasks' queue
     console.log('Sending toggleTask message to RabbitMQ');
     channel.sendToQueue('tasks', Buffer.from(JSON.stringify({ action: 'toggle', id })));
     console.log('toggleTask message sent to RabbitMQ');
 
-    return task;
+    return true;
   },
   deleteTask: async ({ id }) => {
-    await Task.findByIdAndDelete(id);
 
     // Publish a message to the 'tasks' queue
     console.log('Sending deleteTask message to RabbitMQ');
@@ -154,6 +161,7 @@ const root = {
     return true;
   },
 };
+
 
 // Create Express server
 const app = express();
@@ -170,6 +178,21 @@ app.use('/graphql', graphqlHTTP({
   rootValue: root,
   graphiql: true,
 }));
+
+// Middleware to cache tasks in Redis
+app.use('/graphql', async (req, res, next) => {
+  try {
+    const tasks = await Task.find();
+    redisClient.set('tasks', JSON.stringify(tasks), (err) => {
+      if (err) {
+        console.error('Error setting tasks in Redis:', err);
+      }
+    });
+  } catch (error) {
+    console.error('Error caching tasks in Redis:', error);
+  }
+  next();
+});
 
 // Start the server
 const PORT = 4002;
